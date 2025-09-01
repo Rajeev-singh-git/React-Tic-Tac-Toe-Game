470d17035e846935b7bb2a61dd22cda4333d39fb
Rajeev Singh <rajeevsinghece49@gmail.com>
2025-09-01 18:35:46 +0530

    feat : Static player name and symbol

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-
 function App() {
-  
-
   return (
-    <p>Coming Soon ...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span classSymobol="player-name">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span classSymobol="player-name">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
